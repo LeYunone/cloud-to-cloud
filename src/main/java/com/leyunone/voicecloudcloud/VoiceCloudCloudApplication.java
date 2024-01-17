433eb55c3ef4d257879ef4c24c2b59677cae4e20
LeYunone <365627310@qq.com>
2024-01-18 00:09:27 +0800

    Initial commit

---
+++ src/main/java/com/leyunone/voicecloudcloud/VoiceCloudCloudApplication.java
@@ -0,0 +1,13 @@
+package com.leyunone.voicecloudcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceCloudCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceCloudCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leyunone/voicecloudcloud/VoiceCloudCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leyunone.voicecloudcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceCloudCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
